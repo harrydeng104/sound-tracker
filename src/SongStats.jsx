9001cd97f823d5161dccb73c54765155b7c145ab
harrydeng104 <harrydeng104@gmail.com>
2026-03-18 22:51:55 -0400

    comments display

--- src/SongStats.jsx
+++ src/SongStats.jsx
@@ -8,17 +8,19 @@ function SongStats( {song, onClose} ) {
     return (<>
                 <div className = "song-stats">
             <h1>{song.name}</h1>
-            <p>Total {song.totalScore}</p>
+            <p>Total: {song.totalScore}</p>
 
-            <p>Vocals {song.vocalScore}</p>
+            <p>Vocals: {song.vocalScore}</p>
             
-            <p>Background {song.backgroundScore}</p>
+            <p>Background: {song.backgroundScore}</p>
             
-            <p>Lyrics {song.lyricScore}</p>
+            <p>Lyrics: {song.lyricScore}</p>
 
-            <p>Cohesion {song.cohesionScore}</p>
+            <p>Cohesion: {song.cohesionScore}</p>
             
-            <p>Flow {song.flowScore}</p>
+            <p>Flow: {song.flowScore}</p>
+
+            <p>Comments: {song.comments}</p>
 
             <button 
                 className = "close-button"
